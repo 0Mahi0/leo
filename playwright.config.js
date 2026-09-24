2d7c5b892111c9b78359c6783691dfcc9e23dbe3
0Mahi0 <Maheswari_T@outlook.com>
2026-09-24 16:58:04 +0530

    new checkfile

--- playwright.config.js
+++ playwright.config.js
@@ -36,22 +36,15 @@ export default defineConfig({
   reporter: [['json',{outputFile : "JsonReporter.json"}], ['junit', {outputFile : "JunitReport.xml"}]]
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-  
-    trace: 'on',
-    headless : false,
-    baseURL : "https://www.google.com/",
-
-    screenshot : "on",
-    video : "on",
-    viewport : { width : 1280, height : 720 },
-    //actionTimeout : 500,
-    //channel : "msedge"
-
+  trace: 'on',
+  headless: false,
+  baseURL: "https://www.google.com/",
+  screenshot: "on",
+  video: "on",
+  viewport: { width: 1280, height: 720 },
   },
+
+
 
   /* Configure projects for major browsers */
   projects: [
---
+++ tests/checkfile.spec.js
@@ -0,0 +1,30 @@
+
+
+test('Handling Checkboxes', async function({page}) {
+    await page.goto("https://testautomationpractice.blogspot.com/")
+    await page.waitForTimeout(2000)
+
+    // Single checkbox
+    await page.locator('[id="sunday"]').check()
+    await page.waitForTimeout(2000)
+
+    // Multiple checkboxes
+    const multiCheckBoxes = [
+        page.locator('#monday'),
+        page.locator('[id="thursday"]')
+    ]
+
+    // Select multiple checkboxes
+    for (const element of multiCheckBoxes) {
+        await element.check()
+        await page.waitForTimeout(150)
+        await expect(element).toBeChecked()
+    }
+
+    // Unselect multiple checkboxes
+    for (const element of multiCheckBoxes) {
+        await element.uncheck()
+        await page.waitForTimeout(1500)
+        await expect(element).not.toBeChecked()
+    }
+})
