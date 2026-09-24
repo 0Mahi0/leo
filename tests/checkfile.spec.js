ab5aa2c53e678a5de5e2a8503bba64b09ae55018
0Mahi0 <Maheswari_T@outlook.com>
2026-09-24 17:01:03 +0530

    changes in checkfile

--- tests/checkfile.spec.js
+++ tests/checkfile.spec.js
@@ -21,10 +21,6 @@ test('Handling Checkboxes', async function({page}) {
         await expect(element).toBeChecked()
     }
 
-    // Unselect multiple checkboxes
-    for (const element of multiCheckBoxes) {
-        await element.uncheck()
-        await page.waitForTimeout(1500)
-        await expect(element).not.toBeChecked()
-    }
+    
 })
+
